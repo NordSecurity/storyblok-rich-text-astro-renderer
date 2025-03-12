d603625e9c47bf5e9b712361ec8dcb50a75fd713
Edvinas Jurelė <edvinas.j@nordsec.com>
2025-03-12 11:41:25 +0200

    chore: bump astro, storyblok/astro and vitest (#340)
    Signed-off-by: Edvinas Jurele <edvinas.j@nordsec.com>

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import storyblok from "@storyblok/astro";
+import { storyblok } from "@storyblok/astro";
 
 // https://astro.build/config
 export default defineConfig({
